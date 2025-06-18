d49ef1bffbf945fc6ed52c04b1da59381912fdaa
Matheus Augusto <matheusamg@gmail.com>
2025-06-18 16:10:44 -0300

    Criar o left shifter

--- led_shifter.v
+++ led_shifter.v
@@ -6,5 +6,21 @@ module LedShifter #(
     output  reg [7:0] leds
 );
 
+    localparam ONE_SECOND = CLK_FREQ;
+    reg [31:0] counter;
+
+    always @(posedge clk) begin
+        if (!rst_n) begin
+            leds <= 8'b00011111;
+            counter <= 0;
+        end
+        if (counter >= ONE_SECOND - 1)begin
+            counter <= 0;
+            leds <= {leds[6:0], leds[7]};
+        else begin
+            counter <= couter + 1;
+        end
+
+    end
     
 endmodule
