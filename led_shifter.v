be50c224028cd36f72ca8ec84e46f1fbd23b3fe1
Matheus Augusto <matheusamg@gmail.com>
2025-06-18 17:11:27 -0300

    Erro arrumado

--- led_shifter.v
+++ led_shifter.v
@@ -6,21 +6,23 @@ module LedShifter #(
     output  reg [7:0] leds
 );
 
-    localparam ONE_SECOND = CLK_FREQ;
+    localparam QUARTER_SECOND = CLK_FREQ/4;
     reg [31:0] counter;
 
     always @(posedge clk) begin
         if (!rst_n) begin
             leds <= 8'b00011111;
-            counter <= 0;
-        end
-        if (counter >= ONE_SECOND - 1)begin
-            counter <= 0;
-            leds <= {leds[6:0], leds[7]};
+            counter <= 'h0000;
         end
         else begin
-            counter <= couter + 1;
-        end
+            if (counter >= ONE_SECOND - 1)begin
+                counter <= 0;
+                leds <= {leds[6:0], leds[7]};
+            end
+            else begin
+                counter <= counter + 1;
+            end
+        end 
     end
     
 endmodule
